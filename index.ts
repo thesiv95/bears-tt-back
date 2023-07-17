07263b80cb3cd1ad3ddfccb41c356612c4a8ab29
Ilya Soloveychik <dj.active.17@gmail.com>
2023-07-17 14:46:08 +0700

    feat: add express server + mysql db

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log('test')
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import connection from './db/connection';
+
+dotenv.config();
+
+connection.connect(function (err) {
+	if (err) {
+		console.error('DB - error connecting: ' + err.stack);
+		return;
+	}
+	console.log('DB connected');
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const APP_PORT = process.env.APP_PORT || 7000;
+
+app.get('/', (_req, res) => res.send({ status: 'running' }));
+
+app.listen(APP_PORT, () => console.log(`App is listening on port ${APP_PORT}`));
+
